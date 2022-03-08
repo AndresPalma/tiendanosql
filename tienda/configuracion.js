f468bcca59bbfb5eb6350c99ca01675da397a14d
AndresPalma <93955080+AndresPalma@users.noreply.github.com>
2022-03-08 17:15:16 -0500

    Actualizacion de configuracion tienda nosql

--- tienda/configuracion.js
+++ tienda/configuracion.js
@@ -1,5 +1,5 @@
 const config = {
-    DB_URL: 'mongodb+srv://ups:ups2020@cluster0.pjyad.mongodb.net/tienda?retryWrites=true&w=majority',
+    DB_URL: 'mongodb+srv://ups:ups2021@cluster0.lchrd.mongodb.net/tienda?retryWrites=true&w=majority',
     PUERTO: 3000
 }
 
